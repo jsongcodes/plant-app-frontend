5e33bc0b5622609f0173e6dc8aeb60e027684b4f
Jasmin <ssongnari@gmail.com>
2022-11-23 12:07:06 -0500

    Added react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,9 @@
 import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./Navbar";
+import NewPlant from "./NewPlant";
+import PastPlants from "./PastPlants";
+import Home from "./Home";
 
 const App = () => {
   const [plants, setPlants] = useState([]);
@@ -10,7 +15,24 @@ const App = () => {
   }, []);
 
   return (
-    <></>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/plants">
+              <PastPlants plants={plants}/>
+            </Route>
+            <Route path="/newplant">
+              <NewPlant onAddPlant={handleAddPlants} />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 };
 
