78013af47c92e5be3044728574d4d36a22447715
Jasmin <ssongnari@gmail.com>
2022-11-23 12:31:37 -0500

    Homepage initial code

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,20 @@
+import { useHistory } from "react-router-dom";
+
 const Home = () => {
+    const history = useHistory();
+
+    const handleClick = () => {
+        history.push("/newplant");
+      };
+
     return (
-        <>
-        </>
+<div className="home-class">
+      <h2>Need help taking care of your houseplant?</h2>
+
+      <button className="home-class" onClick={handleClick}>
+        Find out how 📝
+      </button>
+    </div>
     )
 }
 
