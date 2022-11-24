0b38a4bde34974bde5fc8b241faaabb60e831d7e
Jasmin <ssongnari@gmail.com>
2022-11-23 19:44:06 -0500

    Added controlled form

--- src/components/NewPlant.js
+++ src/components/NewPlant.js
@@ -1,7 +1,36 @@
+import { useState } from "react";
+
 const NewPlant = () => {
+    const [inputForm, setInputForm] = useState({
+        userName: "",
+        plantName: "",
+        description: "",
+        plantCare: "",
+      });
+    
     return (
-        <>
-        </>
+        <div className="new-plant">
+                 <h2>Add a New Plant</h2>
+      <form onSubmit={handleSubmit}>
+        <label>user name:</label>
+        <textarea
+          name="body"
+          required
+          value={inputForm.userName}
+          onChange={handleChange}
+        />
+
+        <label>plant name:</label>
+        <textarea
+          required
+          name="plantName"
+          value={inputForm.plantName}
+          onChange={handleChange}
+        />
+
+        <button>Add Plant 📝</button>
+      </form> 
+        </div>
     )
 }
 
