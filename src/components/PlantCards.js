3b8a89efa5feef034e4287294533607fa5b5ad37
Jasmin <ssongnari@gmail.com>
2022-11-25 16:13:35 -0500

    Added plant cards

--- src/components/PlantCards.js
+++ src/components/PlantCards.js
@@ -1,7 +1,31 @@
-const PlantCards = () => {
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
+
+const PlantCards = ({plants}) => {
     return (
-        <>
-        </>
+        <div className="entry-list">
+
+        <Container>
+  
+          <Grid container spacing={3}>
+            {plants.map((plant) => (
+              <Grid
+                className="past-entries"
+                item
+                xs={12}
+                md={6}
+                lg={4}
+                key={plant.id}
+              >
+  
+                  <h2>{plant.userName}</h2>
+                  <p className="body">{plant.plantName}</p>
+  
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </div>
     )
 }
 
