45fb8bec3b827e96f2a4e0d0a4d0cdfc53d63e0e
Jasmin <ssongnari@gmail.com>
2022-11-26 20:25:07 -0500

    Deleted Home component

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,13 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./Navbar";
 import NewPlant from "./NewPlant";
 import PlantPage from "./PlantPage";
-import Home from "./Home";
+import Login from "./Login";
 
 const App = () => {
   const [plants, setPlants] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:9292")
+    fetch("http://localhost:9292/plants")
       .then((resp) => resp.json())
       .then((data) => setPlants(data));
   }, []);
@@ -25,14 +25,14 @@ const App = () => {
         <Navbar />
         <div className="content">
           <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/plants">
-              <PlantPage plants={plants}/>
+            <Route path="/">
+              <PlantPage plants={plants} />
             </Route>
             <Route path="/newplant">
               <NewPlant onAddPlant={handleAddPlants} />
+            </Route>
+            <Route path="/login">
+              <Login />
             </Route>
           </Switch>
         </div>
