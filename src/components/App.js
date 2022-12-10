c7c0207cb27fb559e5f7085a742025f36b69ff91
Jasmin <ssongnari@gmail.com>
2022-12-09 20:29:00 -0500

    Created different component folders

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,21 @@
-import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Navbar from "./Navbar";
-import NewPlant from "./NewPlant";
-import PlantPage from "./PlantPage";
-import Login from "./Login";
+import Navbar from "./shared/Navbar";
+import PlantsContainer from "./plants/PlantsContainer";
+import UsersContainer from "./users/UsersContainer";
 
 const App = () => {
-  const [plants, setPlants] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:9292/plants")
-      .then((resp) => resp.json())
-      .then((data) => setPlants(data));
-  }, []);
-
-  function handleAddPlants(newPlant) {
-    const updatedPlants = [...plants, newPlant];
-    setPlants(updatedPlants);
-  }
 
   return (
     <Router>
-      <div className="App">
         <Navbar />
-        <div className="content">
           <Switch>
-            <Route path="/">
-              <PlantPage plants={plants} />
+            <Route path="/plants">
+              <PlantsContainer/>
             </Route>
-            <Route path="/newplant">
-              <NewPlant onAddPlant={handleAddPlants} />
-            </Route>
-            <Route path="/login">
-              <Login />
+            <Route path="/users">
+              <UsersContainer />
             </Route>
           </Switch>
-        </div>
-      </div>
     </Router>
   );
 };
---
+++ src/components/users/UsersContainer.js
@@ -0,0 +1,120 @@
+import React, { useState, useEffect } from 'react';
+import { Switch, Route, useHistory, useLocation } from 'react-router-dom';
+import UsersToolBar from './UsersToolBar';
+import UsersList from './UsersList';
+import NewUserForm from './NewUserForm';
+import UserDetail from './UserDetail';
+import UserEditForm from './UserEditForm';
+
+function UsersContainer() {
+  const history = useHistory();
+  const location = useLocation();
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function fetchUsers() {
+      const res = await fetch(`http://localhost:9292/users`, {
+        headers: { Accept: 'application/json' }
+      });
+
+      const parsedBody = await res.json();
+
+      setUsers(parsedBody);
+    }
+
+    fetchUsers();
+  }, []);
+
+  
+  const addUser = (formData) => {
+    fetch(`https://localhost:9292/users`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+      .then(res => res.json())
+      .then(newUser => {
+        setUsers(users.concat(newUser))
+        history.push(`/${newUser.id}`)
+      });
+  }
+  
+
+  const updateUser = (id, formData) => {
+    fetch(`https://localhost:9292/users/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+      .then(res => res.json()) 
+      .then(updatedUser => {
+        // pessimistically update the dog in state after we get a response from the api
+        setUsers(users.map((user) => (user.id === parseInt(id) ? updatedUser : user)));
+        history.push(`/${updatedUser.id}`);
+      })
+  }
+
+  const deleteUser = (userId) => {
+    if (window.confirm('Are you sure you want to delete this dog?')) {
+      // optimistically update the ui
+      setUsers(users.filter(user => user.id !== parseInt(userId)))
+      // update the API
+      fetch(`https://localhost:9292/users/${userId}`, {
+        method: 'DELETE',
+        headers: { Accept: 'application/json' }
+      })
+        .then(res => res.json())
+        .then(deletedUser => {
+          console.log('deleted', deletedUser.name)
+          if (location.pathname !== "/users") {
+            history.push("/users")
+          }
+        });
+    }
+  }
+
+  return (
+    <div className="w-4/5 mx-auto pt-12">
+      <UsersToolBar />
+      <Switch>
+        <Route exact path="/users">
+          <UsersList users={users} deleteUser={deleteUser} />
+        </Route>
+        <Route exact path="/users/new">
+          <NewUserForm users={users} addUser={addUser} />
+        </Route>
+        <Route
+          exact
+          path="/users/:id"
+          render={({ match }) => (
+            <UserDetail
+            //   togglePoo={togglePoo}
+            //   deleteDogWalk={deleteDogWalk}
+              deleteUser={deleteUser}
+            //   addDogWalk={addDogWalk}
+              user={users.find((user) => user.id === parseInt(match.params.id))}
+            />
+          )}
+        />
+        <Route
+          exact
+          path="/users/:id/edit"
+          render={({ match }) => (
+            <UserEditForm
+              user={users.find((user) => user.id === parseInt(match.params.id))}
+              updateUser={updateUser}
+            />
+          )}
+        />
+      </Switch>
+    </div>
+  );
+}
+
+export default UsersContainer;
