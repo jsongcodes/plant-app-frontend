4f115195bc3266418672635ffa2a9cb0927f0152
Jasmin <ssongnari@gmail.com>
2022-12-20 14:24:09 -0500

    Added useEffect for plants and users

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,80 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Navbar from "./shared/Navbar";
-import PlantsContainer from "./plants/PlantsContainer";
-import UsersContainer from "./users/UsersContainer";
+import { useHistory } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+
+import Navbar from "./Navbar";
+
+import NewUserForm from "./NewUserForm";
+import UserDetail from "./UserDetail";
+import UsersList from "./UsersList";
+
+import PlantsContainer from "./PlantsContainer";
 
 const App = () => {
+  const history = useHistory();
+  
+  const [users, setUsers] = useState([]);
+  const [plants, setPlants] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:9292/users`)
+      .then((resp) => resp.json())
+      .then((users) => setUsers(users));
+  }, []);
+
+  const addUser = (formData) => {
+    fetch(`http://localhost:9292/users`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((res) => res.json())
+      .then((newUser) => {
+        setUsers(users.concat(newUser));
+        console.log(newUser.id)
+        history.push(`/users/${newUser.id}`);
+      });
+  };
+
+
+  // useEffect(() => {
+  //   fetch(`http://localhost:9292/plants`)
+  //     .then((resp) => resp.json())
+  //     .then((plants) => setPlants(plants));
+  // }, []);
+
   return (
     <Router>
       <Navbar />
       <Switch>
-        <Route path="/plants">
-          <PlantsContainer />
+        <Route exact path="/users">
+          <UsersList users={users} />
         </Route>
-        <Route path="/users">
-          <UsersContainer />
+
+        <Route exact path="/users/new">
+          <NewUserForm addUser={addUser} />
+        </Route>
+        <Route
+          exact
+          path="/users/:id"
+          render={({ match }) => (
+            <UserDetail
+           
+              user={users.find((user) => user.id === parseInt(match.params.id))}
+            />
+          )}
+        />
+
+        <Route exact path="/plants">
+          <PlantsContainer
+            plants={plants}
+            setPlants={setPlants}
+            users={users}
+            setUsers={setUsers}
+          />
         </Route>
       </Switch>
     </Router>
