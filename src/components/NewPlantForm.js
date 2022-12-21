ffc90cfb8d8c7e68ac25e8d7b3a94d8a0c214e53
Jasmin <ssongnari@gmail.com>
2022-12-20 19:30:22 -0500

    Deleted user_id from form

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -2,9 +2,11 @@ import { useState, useEffect } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 
-const NewPlantForm = ({ id, onAddPlant }) => {
-  const [userIds, setUserIds] = useState([]);
-  const [users, setUsers] = useState([]);
+const NewPlantForm = ({ 
+  // id, 
+  onAddPlant, setPlants }) => {
+  // const [userIds, setUserIds] = useState([]);
+  // const [users, setUsers] = useState([]);
   const [user_name, setUserName] = useState('');
   const [user_image_url, setUserImageUrl] = useState('');
   const [plant_name, setPlantName] = useState('');
@@ -14,20 +16,15 @@ const NewPlantForm = ({ id, onAddPlant }) => {
   const [water_level, setWaterLevel] = useState('');
   const [sunlight_hours, setSunlightHours] = useState('');
   const [plant_description, setPlantDescription] = useState('');
-  const [city_id, setCityId] = useState(id)
 
-  useEffect(() => {
-    fetch(`http://localhost:9292/plants`)
-      .then((res) => res.json())
-      .then((users) => setUsers(users));
-  }, []);
+
 
 
   const handleSubmit = (e) => {
     e.preventDefault();
     onAddPlant({
-      user_ids: userIds,
-      user_name,
+      // user_ids: userIds,
+      // user_name,
       user_image_url,
       plant_name,
       plant_nickname,
@@ -49,20 +46,6 @@ const NewPlantForm = ({ id, onAddPlant }) => {
       autoComplete="off"
       onSubmit={handleSubmit}
     >
-      <TextField
-        required
-        value={user_name}
-        onChange={(e) => setUserName(e.target.value)}
-        label="user name"
-      />
-      <TextField
-        required
-        id="outlined-required"
-        name="user-image-url"
-        value={user_image_url}
-        onChange={(e) => setUserImageUrl(e.target.value)}
-        label="user image url"
-      />
       <TextField
         required
         id="outlined-required"
@@ -125,48 +108,6 @@ const NewPlantForm = ({ id, onAddPlant }) => {
       
       <button>Add Plant</button>
     </Box>
-
-    // <div className="form">
-    //   <h2>Plants</h2>
-    //   <form onSubmit={handleSubmit}>
-    //     <fieldset>
-    //       <h2>Who are you?</h2>
-    //       <textarea
-    //         name="user"
-    //         required
-    //         value={inputForm.user}
-    //         onChange={handleChange}
-    //       ></textarea>
-    //       <h2>How many years have you been taking care of plants?</h2>
-    //       <textarea
-    //         name="yearsOfExperience"
-    //         required
-    //         value={inputForm.yearsOfExperience}
-    //         onChange={handleChange}
-    //       ></textarea>
-
-    //       <h2>Which plant do you want to write a description about?</h2>
-    // <label>Plants</label>
-    // <div className="grid sm:grid-cols-6 gap-4">
-    //   {users?.map((user) => (
-    //     <label key={user.id}>
-    //       <img src={user.image_url} alt={user.name} title={user.name} />
-    //       {user.name}
-    //       <input
-    //         type="checkbox"
-    //         className="hidden"
-    //         id={`user-${user.id}`}
-    //         value={user.id}
-    //         onChange={handleCheckboxChange}
-    //       />
-    //     </label>
-    //   ))}
-    // </div>
-    //     </fieldset>
-
-    //     <button>Add Plant</button>
-    //   </form>
-    // </div>
   );
 };
 
