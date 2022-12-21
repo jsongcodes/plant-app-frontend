78f1d307cdaa919305cd484ace3296a815e14c3a
Jasmin <ssongnari@gmail.com>
2022-12-20 19:40:12 -0500

    history.push is working

---
+++ src/components/UsersList.js
@@ -0,0 +1,51 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import UserListItem from "./UserListItem";
+import Grid from "@mui/material/Grid"; // Grid version 1
+import { useHistory } from "react-router-dom";
+// import { Link } from "react-router-dom";
+
+
+
+const UsersList = ({ users, plants}) => {
+
+  const history = useHistory();
+
+  return (
+    <div>
+      <h2>Who are you?</h2>
+      <button
+      onClick={() => {
+        history.push("/users/new");
+      }}>add user</button>
+
+      {Array.from(Array).map((_, index) => (
+        <Grid
+          key={index}
+          container
+          direction="row"
+          justifyContent="space-evenly"
+          alignItems="center"
+        >
+          {users.map((user) => (
+            <Card key={user.id} sx={{ maxWidth: 400 }}>
+              <CardMedia
+                component="img"
+                height="280"
+                image={user.user_image_url}
+                alt="user"
+              />
+              <UserListItem
+                key={user.id}
+                user={user}
+              />
+            </Card>
+          ))}
+        </Grid>
+      ))}
+    </div>
+  );
+};
+
+export default UsersList;
