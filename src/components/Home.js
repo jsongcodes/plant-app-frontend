a974a4048d1a55c2a791383cd37dca0d9368e337
Jasmin <ssongnari@gmail.com>
2022-12-20 22:16:20 -0500

    Added image to homepage

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import { useHistory } from "react-router-dom";
+
+const Home = () => {
+    const history = useHistory();
+    return(
+        <div className="home-class">
+        <h1>keep track of your plant care</h1>
+        <div>
+        <img src="https://media.istockphoto.com/id/1009948020/vector/collection-of-different-decor-house-indoor-garden-plants-in-pots-and-stands-graphic-set.jpg?s=612x612&w=0&k=20&c=R1NZ2iDTOIFi2dEKXY5737QiJmWK0RHA_8PjCd8trgY="/></div>
+        <button onClick={() => history.push('/users')}>get started</button>
+        </div>
+    )
+}
+
+export default Home;
