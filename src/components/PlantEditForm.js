ed8523cd799566af35c7b8c81ce04b20efdbaecd
Jasmin <ssongnari@gmail.com>
2022-12-27 18:52:15 -0500

    Changed useState to empty strings

--- src/components/PlantEditForm.js
+++ src/components/PlantEditForm.js
@@ -3,22 +3,19 @@ import { useParams } from "react-router-dom";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 
-const PlantEditForm = ({ plant = {}, updatePlant }) => {
-  const [plant_name, setPlantName] = useState(plant.plant_name);
-  const [plant_nickname, setPlantNickname] = useState(plant.plant_nickname);
-  const [plant_image_url, setPlantImageUrl] = useState(plant.plant_image_url);
-  const [environment, setEnvironment] = useState(plant.plant_environment);
-  const [water_level, setWaterLevel] = useState(plant.water_level);
-  const [sunlight_hours, setSunlightHours] = useState(plant.sunlight_hours);
-  const [plant_description, setPlantDescription] = useState(
-    plant.plant_description
-  );
-
+const PlantEditForm = ({ plant = {}, updatePlant, currentUser }) => {
   const { id } = useParams();
+
+  const [plant_name, setPlantName] = useState("");
+  const [plant_nickname, setPlantNickname] = useState("");
+  const [plant_image_url, setPlantImageUrl] = useState("");
+  const [environment, setEnvironment] = useState("");
+  const [water_level, setWaterLevel] = useState("");
+  const [sunlight_hours, setSunlightHours] = useState("");
+  const [plant_description, setPlantDescription] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
     updatePlant(id, {
       plant_name,
       plant_nickname,
@@ -31,6 +28,7 @@ const PlantEditForm = ({ plant = {}, updatePlant }) => {
   };
 
   useEffect(() => {
+    // set formData to plant
     setPlantName(plant.plant_name);
     setPlantNickname(plant.plant_nickname);
     setPlantImageUrl(plant.plant_image_url);
@@ -44,6 +42,7 @@ const PlantEditForm = ({ plant = {}, updatePlant }) => {
     <>
       <Box
         component="form"
+        className="home-class"
         sx={{
           "& .MuiTextField-root": { m: 1, width: "40ch" },
         }}
@@ -51,71 +50,56 @@ const PlantEditForm = ({ plant = {}, updatePlant }) => {
         autoComplete="off"
         onSubmit={handleSubmit}
       >
-        <h1>Edit User: {plant.plant_name}</h1>
-        <TextField
-          required
-          // type="text"
-          name="plant_name"
-          id="plant_name"
-          // label="plant name"
-          value={plant_name}
-          onChange={(e) => setPlantName(e.target.value)}
-        />
-        <TextField
-          required
-          // type="text"
-          name="plant_nickname"
-          id="plant_nickname"
-          // label="plant nickname"
-          value={plant_nickname}
-          onChange={(e) => setPlantNickname(e.target.value)}
-        />
-        <TextField
-          required
-          // type="text"
-          name="plant_image_url"
-          id="plant_image_url"
-          // label="plant image url"
-          value={plant_image_url}
-          onChange={(e) => setPlantImageUrl(e.target.value)}
-        />
-        <TextField
-          required
-          // type="text"
-          name="environment"
-          id="environment"
-          // label="environment"
-          value={environment}
-          onChange={(e) => setEnvironment(e.target.value)}
-        />
-
-        <TextField
-          required
-          // type="text"
-          name="water_level"
-          id="water_level"
-          // label="environment"
-          value={water_level}
-          onChange={(e) => setWaterLevel(e.target.value)}
-        />
-        <TextField
-          required
-          // type="text"
-          name="sunlight_hours"
-          id="sunlight_hours"
-          // label="sunlight_hours"
-          value={sunlight_hours}
-          onChange={(e) => setSunlightHours(e.target.value)}
-        />
-        <TextField
-          required
-          // type="text"
-          name="plant_description"
-          id="plant_description"
-          // label="plant_description"
-          value={plant_description}
-          onChange={(e) => setPlantDescription(e.target.value)}
-        />
+        <h1>update {plant.plant_name}</h1>
+        <div>
+          <TextField
+            value={plant_name}
+            label="plant name"
+            onChange={(e) => setPlantName(e.target.value)}
+          />
+        </div>
+        <div>
+          <TextField
+            value={plant_nickname}
+            label="plant nickname"
+            onChange={(e) => setPlantNickname(e.target.value)}
+          />
+        </div>
+        <div>
+          <TextField
+            value={plant_image_url}
+            label="plant image url"
+            onChange={(e) => setPlantImageUrl(e.target.value)}
+          />
+        </div>
+        <div>
+          <TextField
+            value={environment}
+            label="environment"
+            onChange={(e) => setEnvironment(e.target.value)}
+          />
+        </div>
+        <div>
+          <TextField
+            value={water_level}
+            label="water level"
+            onChange={(e) => setWaterLevel(e.target.value)}
+          />
+        </div>
+        <div>
+          <TextField
+            value={sunlight_hours}
+            label="sunlight hours"
+            onChange={(e) => setSunlightHours(e.target.value)}
+          />
+        </div>
+        <div>
+          <TextField
+            value={plant_description}
+            label="plant description"
+            onChange={(e) => setPlantDescription(e.target.value)}
+          />
+        </div>
         <button type="submit">update plant</button>
       </Box>
     </>
