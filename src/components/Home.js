6823cef611f00488fcb53c590f2129039f127655
Jasmin <ssongnari@gmail.com>
2022-12-27 18:51:00 -0500

    Added a homepage with image

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,9 @@ const Home = () => {
     const history = useHistory();
     return(
         <div className="home-class">
-        <h1>keep track of your plant care</h1>
+        <h1>welcome, plant parents!</h1>
         <div>
-        <img src="https://media.istockphoto.com/id/1009948020/vector/collection-of-different-decor-house-indoor-garden-plants-in-pots-and-stands-graphic-set.jpg?s=612x612&w=0&k=20&c=R1NZ2iDTOIFi2dEKXY5737QiJmWK0RHA_8PjCd8trgY="/></div>
+        <img src="https://media.istockphoto.com/id/1009948020/vector/collection-of-different-decor-house-indoor-garden-plants-in-pots-and-stands-graphic-set.jpg?s=612x612&w=0&k=20&c=R1NZ2iDTOIFi2dEKXY5737QiJmWK0RHA_8PjCd8trgY=" alt="homepage-image"/></div>
         <button onClick={() => history.push('/users')}>get started</button>
         </div>
     )
