097d80cc3ecf2f2aafebfbf72d06f8a5df23c455
Jasmin <ssongnari@gmail.com>
2022-12-27 18:49:13 -0500

    Added toggleLogout for currentUser

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,48 +1,72 @@
-// import { Link } from "react-router-dom";
 import Link from "@mui/material/Link";
-
 import * as React from "react";
 import Breadcrumbs from "@mui/material/Breadcrumbs";
-// import HomeIcon from "@mui/icons-material/Home";
 import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
-// import YardOutlinedIcon from "@mui/icons-material/YardOutlined";
+import YardOutlinedIcon from "@mui/icons-material/YardOutlined";
 
-const Navbar = () => {
+const Navbar = ({currentUser = {}, setCurrentUser} ) => {
+
+  const toggleLogout = () => {
+    setCurrentUser(null)
+  }
+
+
   return (
-    <>
-      <nav className="navbar">
-        <Link
+    <nav className="navbar">
+      <Link
+        sx={{ display: "flex", alignItems: "center" }}
+        color="inherit"
+        href="/"
+      >
+        <h1 className="title">plantparenthood</h1>
+      </Link>
+      <div className="navbar-links">
+        <Breadcrumbs className="navbar-links" aria-label="breadcrumb">
+          <Link
+            className="navbar-links"
+            sx={{ display: "flex", alignItems: "center" }}
+            color="inherit"
+            href="/plants"
+          >
+            <YardOutlinedIcon sx={{ mr: 0.5 }} fontSize="inherit" />
+            plants
+          </Link>
+
+          {currentUser !== null ?
+           (<Link
+            className="navbar-links"
+            sx={{ display: "flex", alignItems: "center" }}
+            color="inherit"
+            href="/users"
+            onClick={toggleLogout}
+          >
+            <PersonOutlineOutlinedIcon sx={{ mr: 0.5 }} fontSize="inherit" />
+            logout
+          </Link>)
+          :
+         (<Link
+          className="navbar-links"
           sx={{ display: "flex", alignItems: "center" }}
           color="inherit"
-          href="/"
+          href="/users"
         >
-          <h1>plantparenthood</h1>
-        </Link>
+          <PersonOutlineOutlinedIcon sx={{ mr: 0.5 }} fontSize="inherit" />
+          login
+        </Link> )
         
-        <div>
-          <Breadcrumbs aria-label="breadcrumb">
-            {/* <Link
-              underline="hover"
-              sx={{ display: "flex", alignItems: "center" }}
-              color="inherit"
-              href="/plants"
-            >
-              <YardOutlinedIcon sx={{ mr: 0.5 }} fontSize="inherit" />
-              plants
-            </Link> */}
-            <Link
-              underline="hover"
-              sx={{ display: "flex", alignItems: "center" }}
-              color="inherit"
-              href="/users"
-            >
-              <PersonOutlineOutlinedIcon sx={{ mr: 0.5 }} fontSize="inherit" />
-              login
-            </Link>
-          </Breadcrumbs>
-        </div>
-      </nav>
-    </>
+          }
+          
+          <Link
+            className="navbar-links"
+            sx={{ display: "flex", alignItems: "center" }}
+            href="/users/new"
+          >
+            <PersonOutlineOutlinedIcon sx={{ mr: 0.5 }} fontSize="inherit" />
+            register
+          </Link>
+        </Breadcrumbs>
+      </div>
+    </nav>
   );
 };
 
