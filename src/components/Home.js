423de3b189536f695a71f553d87d32b327f6ae3a
Jasmin <ssongnari@gmail.com>
2022-12-27 19:10:41 -0500

    changed alt for image

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
         <div className="home-class">
         <h1>welcome, plant parents!</h1>
         <div>
-        <img src="https://media.istockphoto.com/id/1009948020/vector/collection-of-different-decor-house-indoor-garden-plants-in-pots-and-stands-graphic-set.jpg?s=612x612&w=0&k=20&c=R1NZ2iDTOIFi2dEKXY5737QiJmWK0RHA_8PjCd8trgY=" alt="homepage-image"/></div>
+        <img src="https://media.istockphoto.com/id/1009948020/vector/collection-of-different-decor-house-indoor-garden-plants-in-pots-and-stands-graphic-set.jpg?s=612x612&w=0&k=20&c=R1NZ2iDTOIFi2dEKXY5737QiJmWK0RHA_8PjCd8trgY=" alt="home"/></div>
         <button onClick={() => history.push('/users')}>get started</button>
         </div>
     )
